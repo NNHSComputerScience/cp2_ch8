e67a70998dd3d978b3e58ab83e63fc5408686740
Matt Callaghan <mcallaghan@naperville203.org>
2023-04-05 17:32:58 -0500

    Create classy_critter_starter.py

---
+++ classy_critter_starter.py
@@ -0,0 +1,25 @@
+
+# Classy Critter
+# Demonstrates class attributes and static methods
+
+class Critter:
+    """A virtual pet"""
+    
+    def __init__(self, name):
+        """Initializes a Critter's attributes"""
+        print("A critter has been born!")
+        self.name = name
+
+
+def main():
+    print("Accessing the class attribute Critter.total:", end=" ")
+
+    print("\nCreating critters.")
+
+    print("\nInvoking the static method...")
+
+    print("\nAccessing the class attribute through an object:", end= " ")
+
+main()
+classy_critter_starter.py
+Displaying classy_critter_starter.py.
